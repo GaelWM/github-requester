c0f1d7cb784db4861fb610b1269ad7c5aae17332
Gael Musikingala <gaelmusi0@gmail.com>
2018-07-06 00:29:38 +0200

    Getting Git details API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+
+
 
 class App extends Component {
-  render() {
+  constructor (){
+    super()
+    this.state = {
+      username: '',
+      repos: []
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    axios.get('https://api.github.com/users/gaelwm')
+    .then(response => this.setState({username: response.data.name}))
+
+    axios.get('https://api.github.com/users/gaelwm/repos')
+    .then( response =>  this.setState({ repos: response.data }))
+
+    axios.get('https://api.github.com/repos/gaelwm/github-requester/commits')
+    .then(response => console.log({response}))
+  }
+
+
+  render(){
+
+    const repoNames = this.state.repos.map(function(repo, i) {
+      return <li key={i} style={{ color:'green' }}>{repo.name}</li>
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}> 
+          Click Me
+        </button>
+        <p>{this.state.username}</p>
+        <div>
+          <h1>Repo Names</h1>
+          <ul className='list-group'>
+            {repoNames}
+          </ul>
+        </div>
       </div>
-    );
+      
+    )
   }
 }
 
-export default App;
+export default App
